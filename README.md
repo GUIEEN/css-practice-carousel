883823b7c3e5356a87d05f84e93788736439d5c5
guieenoutis <guieenoutis@gmail.com>
2018-09-04 02:50:08 +0900

    add DOMContentLoaded && add reference site

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 preview: https://guieen.github.io/css-practice-carousel/
+
+CSS site I referred
+* https://gradienthunt.com/
--- src/index.js
+++ src/index.js
@@ -1,54 +1,56 @@
-const nextBtn = document.querySelector('.js-nextBtn'),
-  prevBtn = document.querySelector('.js-prevBtn'),
-  slideList = document.querySelector('.js-slideList'),
-  slides = slideList.querySelectorAll('li'),
-  slideLength = Array.from(slides).length,
-  slider = document.querySelector('.js-slider'),
-  ACTIVE_CLASS = 'active__slide'
+document.addEventListener('DOMContentLoaded', function () {
+  const nextBtn = document.querySelector('.js-nextBtn'),
+    prevBtn = document.querySelector('.js-prevBtn'),
+    slideList = document.querySelector('.js-slideList'),
+    slides = slideList.querySelectorAll('li'),
+    slideLength = Array.from(slides).length,
+    slider = document.querySelector('.js-slider'),
+    ACTIVE_CLASS = 'active__slide'
 
-let currentSlide
-let currentSlideNumber = 1
+  let currentSlide
+  let currentSlideNumber = 1
 
-const calRotate = (slideNum, slideLength) => {
-  slideNum = (((slideNum - 1) % slideLength) + slideLength) % slideLength + 1
-  return slideNum
-}
-
-const getCurrentSlide = () => slideList.querySelector(`li:nth-child(${currentSlideNumber})`)
-
-const toNextSlide = () => {
-  if (currentSlide != null) currentSlide.classList.remove(ACTIVE_CLASS)
-  currentSlide = getCurrentSlide()
-  currentSlide.classList.add(ACTIVE_CLASS)
-}
-
-toNextSlide()
-
-const handle = {
-  prev: () => {
-    currentSlideNumber--
-    currentSlideNumber = calRotate(currentSlideNumber, slideLength)
-    toNextSlide()
-  },
-  next: () => {
-    currentSlideNumber++
-    currentSlideNumber = calRotate(currentSlideNumber, slideLength)
-    toNextSlide()
+  const calRotate = (slideNum, slideLength) => {
+    slideNum = (((slideNum - 1) % slideLength) + slideLength) % slideLength + 1
+    return slideNum
   }
-}
 
-const interval = {
-  kill: () => interval.id != null ? clearInterval(interval.id) : '',
-  start: () => {
-    interval.id = setInterval(handle.next, 1000)
-  },
-  id: null
-}
-interval.start()
+  const getCurrentSlide = () => slideList.querySelector(`li:nth-child(${currentSlideNumber})`)
 
-prevBtn.addEventListener('click', handle.prev)
-nextBtn.addEventListener('click', handle.next)
+  const toNextSlide = () => {
+    if (currentSlide != null) currentSlide.classList.remove(ACTIVE_CLASS)
+    currentSlide = getCurrentSlide()
+    currentSlide.classList.add(ACTIVE_CLASS)
+  }
 
-slider.addEventListener('mouseover', interval.kill)
-slider.addEventListener('mouseout', interval.start)
+  toNextSlide()
+
+  const handle = {
+    prev: () => {
+      currentSlideNumber--
+      currentSlideNumber = calRotate(currentSlideNumber, slideLength)
+      toNextSlide()
+    },
+    next: () => {
+      currentSlideNumber++
+      currentSlideNumber = calRotate(currentSlideNumber, slideLength)
+      toNextSlide()
+    }
+  }
+
+  const interval = {
+    kill: () => interval.id != null ? clearInterval(interval.id) : '',
+    start: () => {
+      interval.id = setInterval(handle.next, 1000)
+    },
+    id: null
+  }
+  interval.start()
+
+  prevBtn.addEventListener('click', handle.prev)
+  nextBtn.addEventListener('click', handle.next)
+
+  slider.addEventListener('mouseover', interval.kill)
+  slider.addEventListener('mouseout', interval.start)
 // prevBtn.onclick = handle.prev
+}, false)
